c4198f760896ade5c07cee6557954c2ef9f86790
nicbudd <nicbudd@nicbudd.com>
2025-03-17 22:08:14 -0400

    repo cleanup

--- target/debug/c16b64.d
+++
@@ -1,1 +0,0 @@
-/stuff/Programming/c16b64/target/debug/c16b64: /stuff/Programming/c16b64/src/main.rs
--- target/release/c16b64.d
+++
@@ -1,1 +0,0 @@
-/home/nicbudd/Programming/c16b64/target/release/c16b64: /home/nicbudd/Programming/c16b64/src/instructions.rs /home/nicbudd/Programming/c16b64/src/main.rs
